82edd824c6482523c5e3a3ca67787a10b279c0a2
Srujan Reddy <srjnreddy33@gmail.com>
2024-12-16 22:23:10 -0600

    added K8s files

--- internal/services/alpha_vantage.go
+++ internal/services/alpha_vantage.go
@@ -4,19 +4,22 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"io"
 	"log"
 	"net/http"
 	"project_fundata/internal/db"
+	"strconv"
 	"time"
 )
 
 type Dividend struct {
-	ExDividendDate  string `json:"ex_dividend_date"`
-	DeclarationDate string `json:"declaration_date"`
-	RecordDate      string `json:"record_date"`
-	PaymentDate     string `json:"payment_date"`
-	Amount          string `json:"amount"`
+	ExDividendDate  time.Time `json:"ex_dividend_date"`
+	DeclarationDate time.Time `json:"declaration_date"`
+	RecordDate      time.Time `json:"record_date"`
+	PaymentDate     time.Time `json:"payment_date"`
+	Amount          float64   `json:"amount"`
 }
 
 type DividendResponse struct {
@@ -46,11 +49,75 @@ func GetDividendInformation(symbol, apiKey, function string) (*DividendResponse,
 		return nil, fmt.Errorf("error reading response body: %v", err)
 	}
 
-	var dividendResponse DividendResponse
-	err = json.Unmarshal(body, &dividendResponse)
+	var rawResponse struct {
+		Symbol string                   `json:"symbol"`
+		Data   []map[string]interface{} `json:"data"`
+	}
+	err = json.Unmarshal(body, &rawResponse)
 	if err != nil {
 		log.Printf("Error unmarshalling response: %v", err)
 		return nil, fmt.Errorf("error unmarshalling response: %v", err)
+	}
+
+	var dividendResponse DividendResponse
+	dividendResponse.Symbol = rawResponse.Symbol
+	defaultDate := time.Date(1970, 1, 1, 0, 0, 0, 0, time.UTC)
+	for _, rawDividend := range rawResponse.Data {
+		var exDividendDate, declarationDate, recordDate, paymentDate time.Time
+		if rawDividend["ex_dividend_date"].(string) != "None" {
+			exDividendDate, err = time.Parse("2006-01-02", rawDividend["ex_dividend_date"].(string))
+			if err != nil {
+				log.Printf("Error parsing ex_dividend_date: %v", err)
+				exDividendDate = defaultDate
+			}
+		} else {
+			exDividendDate = defaultDate
+		}
+		if rawDividend["declaration_date"].(string) != "None" {
+			declarationDate, err = time.Parse("2006-01-02", rawDividend["declaration_date"].(string))
+			if err != nil {
+				log.Printf("Error parsing declaration_date: %v", err)
+				declarationDate = defaultDate
+			}
+		} else {
+			declarationDate = defaultDate
+		}
+		if rawDividend["record_date"].(string) != "None" {
+			recordDate, err = time.Parse("2006-01-02", rawDividend["record_date"].(string))
+			if err != nil {
+				log.Printf("Error parsing record_date: %v", err)
+				recordDate = defaultDate
+			}
+		} else {
+			recordDate = defaultDate
+		}
+		if rawDividend["payment_date"].(string) != "None" {
+			paymentDate, err = time.Parse("2006-01-02", rawDividend["payment_date"].(string))
+			if err != nil {
+				log.Printf("Error parsing payment_date: %v", err)
+				paymentDate = defaultDate
+			}
+		} else {
+			paymentDate = defaultDate
+		}
+		amountStr, ok := rawDividend["amount"].(string)
+		if !ok {
+			log.Printf("Error converting amount to string: %v", rawDividend["amount"])
+			continue
+		}
+		amount, err := strconv.ParseFloat(amountStr, 64)
+		if err != nil {
+			log.Printf("Error converting amount to float: %v", err)
+			continue
+		}
+		dividend := Dividend{
+			ExDividendDate:  exDividendDate,
+			DeclarationDate: declarationDate,
+			RecordDate:      recordDate,
+			PaymentDate:     paymentDate,
+			Amount:          amount,
+		}
+		dividendResponse.Data = append(dividendResponse.Data, dividend)
 	}
 
 	if len(dividendResponse.Data) == 0 {
@@ -72,9 +139,18 @@ func insertDividendData(data DividendResponse) error {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
-	_, err := collection.InsertOne(ctx, data)
+	filter := bson.M{"symbol": data.Symbol}
+	update := bson.M{
+		"$set": bson.M{
+			"symbol": data.Symbol,
+			"data":   data.Data,
+		},
+	}
+	opts := options.Update().SetUpsert(true)
+
+	_, err := collection.UpdateOne(ctx, filter, update, opts)
 	if err != nil {
-		log.Printf("Error inserting data into MongoDB: %v\n", err)
+		log.Printf("Error inserting/updating data into MongoDB: %v\n", err)
 		return fmt.Errorf("error inserting data into MongoDB: %v", err)
 	}
 
